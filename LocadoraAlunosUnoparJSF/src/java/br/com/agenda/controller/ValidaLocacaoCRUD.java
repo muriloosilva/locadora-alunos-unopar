15a97edd5c82535178f1aabc5d6cbc506921a1c1
muriloosilva@gmail.com <muriloosilva@gmail.com@d9ea2f99-9477-45b2-8ef7-11df85a7037c>
2013-02-23 23:24:41 +0000

    Comit

---
+++ LocadoraAlunosUnoparJSF/src/java/br/com/agenda/controller/ValidaLocacaoCRUD.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.agenda.controller;
+
+import br.com.agenda.banco.LocacaoDAO;
+import br.com.agenda.modal.Locacao;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author paulo.terra
+ */
+@RequestScoped
+@ManagedBean(name = "pedido")
+public class ValidaLocacaoCRUD {
+
+    private Locacao locacao;
+
+    public ValidaLocacaoCRUD() {
+        locacao = new Locacao();
+    }
+
+    public String locarOk() {
+
+        String ret = "erro";
+        LocacaoDAO locacaoDAO = new LocacaoDAO();
+        if (locacaoDAO.addLocacao(locacao) == true) {
+            ret = "sucesso";
+        } else {
+            ret = "erro";
+        }
+        return ret;
+    }
+
+    public Locacao getLocacao() {
+        return locacao;
+    }
+    public void setLocacao(Locacao locacao) {
+        this.locacao = locacao;
+    }
+}
